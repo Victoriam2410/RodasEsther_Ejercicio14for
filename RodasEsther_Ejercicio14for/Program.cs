a662ba2866f9de2d68079011dcd2f3ce3413abb5
Victoriam2410 <estherrodas61@gmail.com>
2026-02-24 17:06:20 -0600

    terminado

--- RodasEsther_Ejercicio14for/Program.cs
+++ RodasEsther_Ejercicio14for/Program.cs
@@ -1,4 +1,8 @@
-﻿for (int i = 1; i <= 3; i++)
+﻿int x = 0;
+for (int i = 1; i <= 3; i++)
 {
     Console.WriteLine("ingrese número: ");
+    int numero = int.Parse(Console.ReadLine());
+    x = x + numero;
 }
+Console.WriteLine($"total: {x}");
